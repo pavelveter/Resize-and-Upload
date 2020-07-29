d23d369963d69736e2e264b60ff69aeb3626ac5b
Pavel Veter <kayjaylitee@gmail.com>
2020-07-29 20:25:46 +0300

    Adding text information thru conversion

--- resize.go
+++ resize.go
@@ -33,21 +33,25 @@ const (
 	default_out_width     = 1920
 	default_out_height    = 1920
 	default_compress_rate = 89
+    attention             = "ATTENTION: "
 )
 
 var (
-	out_width     uint
-	out_height    uint
-	on_big_size   bool
+	out_width uint
+	out_height uint
 	compress_rate uint
-	from_dir      string
-	out_dir       string
+	from_dir string
+	out_dir string
+
+    total_files_processed uint
+    total_from_files_size uint
+    total_out_files_size uint
 )
 
 // Надоело писать ифы
 func isFatal(message string, err interface{}) {
     if err != nil {
-        log.Fatalf("ATTENTION:" + message + " %v", err)
+        log.Fatalf(attention + message + " %v", err)
     }
 }
 
@@ -61,19 +65,18 @@ func parseCommandLineFlags() {
 	flag.Parse()
 
 	if flag.NFlag() == 0 {
-		fmt.Println("ATTENTION: No Flags. We use defaults.")
+		fmt.Println(attention + "No Flags. We use defaults.")
+        fmt.Printf("Trying to convert image to fit in rectange %vx%v\n", out_width, out_height)
+        fmt.Println("Input Directory:", from_dir)
+        fmt.Println("Output Directory:", out_dir)
+        fmt.Printf("JPEG Quality: %v%%\n\n", compress_rate)
 	}
-
-	if out_height == out_width {
-		on_big_size = true
-	}
-	return
 }
 
-// Проверяем на допустимость переменные.
+// Проверяем на допустимость флаги командной строки.
 func checkVarsForValidity() {
 	if compress_rate <= 9 || compress_rate >= 101 {
-		fmt.Println("ATTENTION: Compress rate must be in should be in the range of 10 to 100")
+        fmt.Println(attention + "Compress rate must be in the range of 10 to 100")
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -82,10 +85,18 @@ func checkVarsForValidity() {
 // Проверяем, есть ли директория, куда сливать фотки, если нет — создаём
 func checkOutputDir() {
     if _, err := os.Stat(out_dir); os.IsNotExist(err) {
-        fmt.Println("ATTENTION: Directory '" + out_dir + "' not found and will be created.")
+        fmt.Println(attention + "Directory '" + out_dir + "' not found and will be created.")
         err := os.Mkdir(out_dir, 0755)
         isFatal("Failed to make directory " + out_dir, err)
     }
+}
+
+// вычисляет размер файла
+func sizeOfFile(fname string) uint {
+    fi, err := os.Stat(fname)
+    isFatal("Failed to get filesize of " + fname, err)
+
+    return uint(fi.Size())
 }
 
 // Делаем ресайз одной картинки
@@ -93,12 +104,20 @@ func doResizeOneImage(fname string) {
     srcImage, err := imaging.Open(from_dir + dir_separator + fname, imaging.AutoOrientation(true))
     isFatal("Failed to open image: " + fname + " from " + from_dir, err)
 
+  // imaging.Fit — чтобы влезло в прямоугольник заданной ширины
     dstImage := imaging.Fit(srcImage, int(out_width), int(out_height), imaging.Lanczos)
 
     err = imaging.Save(dstImage, out_dir + dir_separator + fname, imaging.JPEGQuality(int(compress_rate)))
     isFatal("Failed to save image:" + fname + " to " + out_dir , err)
 
-  //  fmt.Println(
+    from_file_size := sizeOfFile(from_dir + dir_separator + fname)
+    out_file_size := sizeOfFile(out_dir + dir_separator + fname)
+
+    total_files_processed += 1
+    total_from_files_size += from_file_size
+    total_out_files_size += out_file_size
+
+    fmt.Println(fname + " processed.", from_file_size / 1024,"kib ->", out_file_size / 1024,"kib")
 }
 
 // Проверяем, что картинка по расширению — картинка
@@ -113,11 +132,11 @@ func isItPictureExtension(fname string) bool {
 // Сканируем директорию, откуда надо забирать файлы изображений
 func doFromDirScan() {
     f, err := os.Open(from_dir)
-    isFatal("Can't open directory" + from_dir, err)
+    isFatal("Can't open directory " + from_dir, err)
 
     files, err := f.Readdir(-1)
     f.Close()
-    isFatal("Can't read directory", err)
+    isFatal("Can't read directory " + from_dir, err)
 
     for _, file := range files {
         if isItPictureExtension(file.Name()) {
@@ -131,4 +150,6 @@ func main() {
 	checkVarsForValidity()
     checkOutputDir()
     doFromDirScan()
+
+    fmt.Printf("\nTOTAL: %v files processed, %v mib -> %v mib\n", total_files_processed, total_from_files_size / 1024 / 1024, total_out_files_size / 1024 / 1024)
 }
