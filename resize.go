59f6afea2e5a968ee0126d9bca89d60fbd852947
Pavel Veter <kayjaylitee@gmail.com>
2020-07-29 21:46:52 +0300

    Added timer

--- resize.go
+++ resize.go
@@ -21,19 +21,23 @@ import (
 	"os"
     "path/filepath"
     "strings"
-	//"image"
-	//"image/jpeg"
-	"github.com/disintegration/imaging"
+	"time"
+    "github.com/disintegration/imaging"
 )
 
 const (
 	default_out_dir       = "1. Для просмотра и интернета"
-    default_from_dir      = "2. Для дизайна и печати"
+    default_from_dir      = "2. Для печати и дизайна"
     dir_separator         = "/"
 	default_out_width     = 1920
 	default_out_height    = 1920
 	default_compress_rate = 89
     attention             = "ATTENTION: "
+    inp_dir_s             = "input directory of original images"
+    out_dir_s             = "output directory for resized images"
+    compr_rate_s          = "jpeg compression rate"
+    res_max_width_s       = "resized image max width"
+    res_max_height_s      = "resized image max height"
 )
 
 var (
@@ -48,32 +52,32 @@ var (
     total_out_files_size uint
 )
 
-// Надоело писать ифы
+// Standart if check
 func isFatal(message string, err interface{}) {
     if err != nil {
         log.Fatalf(attention + message + " %v", err)
     }
 }
 
-// Получаем флаги командной строки и инициализируем переменные
+// It gets the command line flags and initializes the variables. 
 func parseCommandLineFlags() {
-	flag.UintVar(&out_width, "w", default_out_width, "resized image width")
-	flag.UintVar(&out_height, "h", default_out_height, "resized image height")
-	flag.StringVar(&out_dir, "o", default_out_dir, "output directory for resized images")
-	flag.StringVar(&from_dir, "i", default_from_dir, "input directory of original images")
-	flag.UintVar(&compress_rate, "c", default_compress_rate, "jpeg compression rate")
+	flag.UintVar(&out_width, "w", default_out_width, res_max_width_s)
+	flag.UintVar(&out_height, "h", default_out_height, res_max_height_s)
+	flag.StringVar(&out_dir, "o", default_out_dir, out_dir_s)
+	flag.StringVar(&from_dir, "i", default_from_dir, inp_dir_s)
+	flag.UintVar(&compress_rate, "c", default_compress_rate, compr_rate_s)
 	flag.Parse()
 
 	if flag.NFlag() == 0 {
 		fmt.Println(attention + "No Flags. We use defaults.")
-        fmt.Printf("Trying to convert image to fit in rectange %vx%v\n", out_width, out_height)
-        fmt.Println("Input Directory:", from_dir)
-        fmt.Println("Output Directory:", out_dir)
-        fmt.Printf("JPEG Quality: %v%%\n\n", compress_rate)
+        fmt.Printf("%s: %v, %s: %v\n", res_max_width_s, out_width, res_max_height_s, out_height)
+        fmt.Printf(inp_dir_s + ": %s\n", from_dir)
+        fmt.Printf(out_dir_s + ": %s\n", out_dir)
+        fmt.Printf(compr_rate_s + ": %v%%\n\n", compress_rate)
 	}
 }
 
-// Проверяем на допустимость флаги командной строки.
+// Let's check the validity of the command line flags. 
 func checkVarsForValidity() {
 	if compress_rate <= 9 || compress_rate >= 101 {
         fmt.Println(attention + "Compress rate must be in the range of 10 to 100")
@@ -82,16 +86,16 @@ func checkVarsForValidity() {
 	}
 }
 
-// Проверяем, есть ли директория, куда сливать фотки, если нет — создаём
+// See if there's a directory to write pictures of, if not, we create it. 
 func checkOutputDir() {
     if _, err := os.Stat(out_dir); os.IsNotExist(err) {
-        fmt.Println(attention + "Directory '" + out_dir + "' not found and will be created.")
+        fmt.Printf(attention + "Directory '%s' not found and will be created.\n", out_dir)
         err := os.Mkdir(out_dir, 0755)
         isFatal("Failed to make directory " + out_dir, err)
     }
 }
 
-// вычисляет размер файла
+// Return filesize
 func sizeOfFile(fname string) uint {
     fi, err := os.Stat(fname)
     isFatal("Failed to get filesize of " + fname, err)
@@ -99,12 +103,12 @@ func sizeOfFile(fname string) uint {
     return uint(fi.Size())
 }
 
-// Делаем ресайз одной картинки
+// Do resize one picture and do some statistics
 func doResizeOneImage(fname string) {
     srcImage, err := imaging.Open(from_dir + dir_separator + fname, imaging.AutoOrientation(true))
     isFatal("Failed to open image: " + fname + " from " + from_dir, err)
 
-  // imaging.Fit — чтобы влезло в прямоугольник заданной ширины
+  // imaging.Fit — resize to fit in rectangle
     dstImage := imaging.Fit(srcImage, int(out_width), int(out_height), imaging.Lanczos)
 
     err = imaging.Save(dstImage, out_dir + dir_separator + fname, imaging.JPEGQuality(int(compress_rate)))
@@ -117,10 +121,10 @@ func doResizeOneImage(fname string) {
     total_from_files_size += from_file_size
     total_out_files_size += out_file_size
 
-    fmt.Println(fname + " processed.", from_file_size / 1024,"kib ->", out_file_size / 1024,"kib")
+    fmt.Printf("%s processed, %vk -> %vk.\n", fname, from_file_size / 1024, out_file_size / 1024)
 }
 
-// Проверяем, что картинка по расширению — картинка
+// Is the right extension for the picture? 
 func isItPictureExtension(fname string) bool {
     switch ext := strings.ToLower(filepath.Ext(fname)); ext {
         case ".jpg": return true
@@ -129,7 +133,7 @@ func isItPictureExtension(fname string) bool {
     }
 }
 
-// Сканируем директорию, откуда надо забирать файлы изображений
+// Let's scan the directory where we need to pick up the image files. 
 func doFromDirScan() {
     f, err := os.Open(from_dir)
     isFatal("Can't open directory " + from_dir, err)
@@ -146,10 +150,12 @@ func doFromDirScan() {
 }
 
 func main() {
-	parseCommandLineFlags()
+    t0 := time.Now()
+
+    parseCommandLineFlags()
 	checkVarsForValidity()
     checkOutputDir()
     doFromDirScan()
 
-    fmt.Printf("\nTOTAL: %v files processed, %v mib -> %v mib\n", total_files_processed, total_from_files_size / 1024 / 1024, total_out_files_size / 1024 / 1024)
+    fmt.Printf("\nTOTAL: %v files processed, %vM -> %vM. Took time: %3v\n", total_files_processed, total_from_files_size / 1024 / 1024, total_out_files_size / 1024 / 1024, time.Since(t0))
 }
